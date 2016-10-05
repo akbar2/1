e1f10dcd48ad41afd6acde078b4ceca29c61875f
akbar2 <fataltrix@outlook.com>
2016-10-05 17:15:44 +0300

    Update README.sql

--- README.sql
+++ README.sql
@@ -1,4 +1,5 @@
-SELECT student_id FROM payments
-GROUP BY student_id
-ORDER BY SUM(amount) DESC
-LIMIT 1,1
+SELECT name, surname
+FROM student s
+JOIN student_status ss ON (s.id = ss.student_id)
+JOIN (SELECT max(id) AS id FROM student_status GROUP BY student_id) maxid ON (maxid.id = ss.id)
+WHERE gender = 'unknown' AND status = 'vacation'
