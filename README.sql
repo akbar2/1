c3e5466112643ee9d11621e3e118589e18392b42
akbar2 <fataltrix@outlook.com>
2016-10-05 17:16:19 +0300

    Update README.sql

--- README.sql
+++ README.sql
@@ -2,4 +2,5 @@ SELECT name, surname
 FROM student s
 JOIN student_status ss ON (s.id = ss.student_id)
 JOIN (SELECT max(id) AS id FROM student_status GROUP BY student_id) maxid ON (maxid.id = ss.id)
-WHERE gender = 'unknown' AND status = 'vacation'
+JOIN (SELECT student_id FROM payments WHERE amount != 0 GROUP BY student_id HAVING count(*) <= 3) p ON (p.student_id = s.id)
+WHERE status = 'lost'
